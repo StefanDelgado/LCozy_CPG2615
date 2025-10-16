94bf33d81de47a5b33028de944b14072a72e05ae
JCgr3y <166883741+JCgr3y@users.noreply.github.com>
2025-10-16 14:18:14 +0800

    reports fix 1

--- Main/modules/reports.php
+++ Main/modules/reports.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/../config.php';
-require_once __DIR__ . '/../auth.php';
+require_once __DIR__ . '../config.php';
+require_once __DIR__ . '../auth.php';
 require_role('admin');
 
 // Date range filter
