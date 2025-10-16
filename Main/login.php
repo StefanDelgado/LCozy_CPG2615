94113ed10fa7d8ca34898f6fab105618298360d5
JCgr3y <166883741+JCgr3y@users.noreply.github.com>
2025-10-16 14:11:31 +0800

    reports test 1

--- Main/login.php
+++ Main/login.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/config.php';
-require_once __DIR__ . '/auth.php';
+require_once __DIR__ . 'config.php';
+require_once __DIR__ . 'auth.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
